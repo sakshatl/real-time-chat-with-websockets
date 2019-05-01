810e74ce0d40d22dcde04fdc9aa7c34020ac93ee
Sakshat <voteforsakshat@gmail.com>
2019-05-01 18:57:59 +0530

    app working wohoo!!

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+const express = require('express');
+const socket = require('socket.io');
 
+let app = express();
+
+app.use(express.static('./public'))
+
+let server = app.listen(5000);
+
+let io = socket(server);
+
+io.on('connection', function(socket){
+    console.log('socket connection made', socket.id);
+    
+    socket.on('chat', function(data){
+        io.emit('chat', data)
+    })
+
+});
 
